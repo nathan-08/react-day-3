0250689dac50db1e0b61fecb80fb46bdf7fc4fd6
Kyle <kylesclawson@gmail.com>
2018-08-16 12:24:29 -0600

    search component

---
+++ src/Components/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Search () {
+    return (
+        <div>
+            <h2>Search</h2>
+            <input/>
+            <select>
+                <option></option>
+                <option></option>
+                <option></option>
+                <option></option>
+            </select>
+        </div>
+    );
+};
+
+export default Search;
