2b54b007a837b4f0a497d7349f7211fa0681d84d
Kyle <kylesclawson@gmail.com>
2018-08-16 11:47:35 -0600

    change quantity

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -5,7 +5,13 @@ function CartItem(props) {
     return (
         <div>
             <h4>{item.name}</h4>
+            <div>
+                <button onClick={() => props.changeQuantity(item, -1)}>-</button>
+                <p>Quantity: {item.quantity}</p>
+                <button onClick={() => props.changeQuantity(item, 1)}>+</button>
+            </div>
             <p>{item.price}</p>
+            <button onClick={() => props.removeItem(item.id)}>Remove Item</button>
         </div>
     );
 
