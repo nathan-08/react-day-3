0fa47fc728cbd38ffa03c6ee7dc6154fef3dc686
Kyle <kylesclawson@gmail.com>
2018-08-16 10:18:48 -0600

    Added clarifying headers and category functionality

--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -41,6 +41,7 @@ class AddProduct extends Component {
         const { nameInput, descriptionInput, priceInput, imageInput, categoryInput } = this.state;
         return (
             <div>
+                <h2>Add Product</h2>
                 <p>Name: </p>
                 <input onChange={e => this.handleName(e.target.value)} value={nameInput}/>
                 <p>Description: </p>
@@ -49,13 +50,14 @@ class AddProduct extends Component {
                 <input onChange={e => this.handlePrice(e.target.value)} value={priceInput}/>
                 <p>Image: </p>
                 <input onChange={e => this.handleImage(e.target.value)} value={imageInput}/>
+                <p>Category: </p>
                 <select onChange={e => this.handleCategory(e.target.value)} value={categoryInput}>
                     <option value="camping">Camping</option>
                     <option value="candy">Candy</option>
                     <option value="clothing">Clothing</option>
                     <option value="shoes">Shoes</option>
                 </select>
-                <button onClick={() => this.props.addProduct({name:nameInput, description:descriptionInput, price:priceInput, imageUrl:imageInput, category:categoryInput})}>Submit</button>
+                <button onClick={() => this.props.addProduct({name:nameInput, description:descriptionInput, price:priceInput, image:imageInput, category:categoryInput})}>Submit</button>
             </div>
         );
     }
