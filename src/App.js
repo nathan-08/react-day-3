80bf9e65771fa09f4efe016f342b3f789443ecc4
nathan-08 <nathanryan001@gmail.com>
2018-08-30 11:20:55 -0600

    update api

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ class App extends Component {
       alert("cart is empty.");
     } else {
       alert("Here's yer stuff");
-      axios.delete("api/checkout?key=" + this.state.apiKey).then(checkoutResponse => {
+      axios.delete("/products/cart/checkout?key=" + this.state.apiKey).then(checkoutResponse => {
         this.setState({
           cart: checkoutResponse.data,
           addressInput: "",
